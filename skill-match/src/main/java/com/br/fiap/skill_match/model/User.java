eabb238d71580caafabe5d6e83097318390cf03f
Henrique <henrique.garcia.cap698@gmail.com>
2025-04-03 01:05:07 -0300

    User

---
+++ skill-match/src/main/java/com/br/fiap/skill_match/model/User.java
@@ -0,0 +1,56 @@
+package com.br.fiap.skill_match.model;
+
+public class User {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+
+    // Construtor padrão
+    public User() {
+    }
+
+    // Construtor com campos
+    public User(Long id, String nome, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
+
